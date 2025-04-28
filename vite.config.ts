9c9ff616e1901f9f604d5046566ff580c54a3f71
harischandio70 <harischandio70@gmail.com>
2025-04-29 02:29:17 +0500

    fix: host

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
   },
   resolve: {
